3838bf0241be770f45dd3a885e15de64bcff9fc1
Jon Young <jey6639@gmail.com>
2023-05-15 23:55:29 -0500

    updated htlm file and finalized for the day

--- demo.js
+++ demo.js
@@ -1,20 +1,33 @@
-'use strict';
+"use strict";
 
-let username = prompt('What is your name?);
-console.log('Hello' + '' + username);
+let username = prompt("What is your name?");
+console.log("Hello" + "" + username);
 
-let time = prompt('What hour is it? (0-23)');
+let time = prompt("What hour is it? (0-23)");
 
-let message = 'Hello';
+let message = "Hello";
 let x = 5;
 x ** 5;
 
 if (time <= 11) {
-    message = 'Good morning'
-} else if (time <=18) {
-    message = 'Good afternoon'
-} else if (time <24) {
-    message = 'Good evening'
+  message = "Good morning";
+} else if (time <= 18) {
+  message = "Good afternoon";
+} else if (time < 24) {
+  message = "Good evening";
 } else {
-    message = 'That hour does not exist'
+  message = "That hour does not exist";
+}
+
+let goodday = prompt("Are you having a good day?");
+console.log(goodday);
+
+if (goodday === "yes") {
+  alert("Excellent");
+} else {
+  alert("That answer is unacceptable, please try again ");
+  goodday = prompt("Are you having a good day?");
+  if (goodday === "yes") {
+    alert("Excellent");
+  }
 }
