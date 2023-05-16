e9cb96e60a832bb2826a384e705ba672368e7e05
Jon Young <jey6639@gmail.com>
2023-05-15 23:03:57 -0500

    added js file for index html

--- demo.js
+++ demo.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let username = prompt('What is your name?);
+console.log('Hello' + '' + username);
+
+let time = prompt('What hour is it? (0-23)');
+
+let message = 'Hello';
+let x = 5;
+x ** 5;
+
+if (time <= 11) {
+    message = 'Good morning'
+} else if (time <=18) {
+    message = 'Good afternoon'
+} else if (time <24) {
+    message = 'Good evening'
+} else {
+    message = 'That hour does not exist'
+}
