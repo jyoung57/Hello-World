2016b09cc5631b39b89cfcc5a34ab11a6abc5858
Jon Young <jey6639@gmail.com>
2023-05-17 16:30:05 -0500

    finalized index htlm file

--- demo.js
+++ demo.js
@@ -1,33 +1,22 @@
 "use strict";
 
-let username = prompt("What is your name?");
+let username = prompt("Welcome to my page. What is your name?");
 console.log("Hello" + "" + username);
 
-let time = prompt("What hour is it? (0-23)");
-
-let message = "Hello";
-let x = 5;
-x ** 5;
-
-if (time <= 11) {
-  message = "Good morning";
-} else if (time <= 18) {
-  message = "Good afternoon";
-} else if (time < 24) {
-  message = "Good evening";
-} else {
-  message = "That hour does not exist";
+let goodday=""
+while (!(goodday ==="Yes"||goodday==="yes")) {  
+  goodday = prompt("Are you having a good day?");
 }
-
-let goodday = prompt("Are you having a good day?");
 console.log(goodday);
 
-if (goodday === "yes") {
-  alert("Excellent");
+function football () {
+  let message = "";
+  let football = prompt("Do you like Football?");
+if (football === "Yes") {
+  message = "That is my favorite sport";
 } else {
-  alert("That answer is unacceptable, please try again ");
-  goodday = prompt("Are you having a good day?");
-  if (goodday === "yes") {
-    alert("Excellent");
-  }
+  message = "What's wrong with you?";
 }
+alert (message)
+}
+  
